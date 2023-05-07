3b520543c6c21f5671a0ab6160da006595f837d1
JackSinnott <c00242525@itcarlow.ie>
2023-05-07 14:00:32 +0100

    NL-16 Enemy spawns off right side of screen initially

---
+++ 2DEnemyBehaviours/Assets/Scripts/InvincibleEnemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvincibleEnemy : MonoBehaviour
+{
+    private Vector3 _pos;
+
+    public float _speed;
+    [SerializeField] private float _frequency;
+    [SerializeField] private float _magnitude;
+
+    [SerializeField] private float _maxBounds;
+    [SerializeField] private float _minBounds;
+
+    private void Start()
+    {
+        _pos = new Vector3(12,Random.Range(_minBounds, _maxBounds),0);
+        transform.position = _pos;
+    }
+
+    private void Update()
+    {
+        //MoveLeft();
+        //if(_pos.x < -12)
+        //{
+        //    this.gameObject.SetActive(false);
+        //}
+    }
+
+    //void MoveLeft()
+    //{
+    //    _pos -= transform.right * Time.deltaTime * _speed;
+    //    transform.position = _pos + transform.up * Mathf.Sin(Time.time * _frequency) * _magnitude;
+    //}
+
+}
